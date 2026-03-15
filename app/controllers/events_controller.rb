8a9a0dc4dd6c1fc9bb9fd58853748e4eaa036f25
SP <nk.montakan@gmail.com>
2026-03-15 14:58:04 +0700

    Add redirect after create with only name

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,18 @@
 class EventsController < ApplicationController
+  def index
+  end
+
   def new
     @event = Event.new
   end
+
   def create
     Event.create(event_params) 
+    redirect_to events_path
   end
+
   private
+
   def event_params
     params.expect(event: [ :name ])
   end
