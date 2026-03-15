424cc086be0874fa551167c9d113cd23d4adba17
SP <nk.montakan@gmail.com>
2026-03-15 14:35:34 +0700

    Create new event with only name

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,12 @@
 class EventsController < ApplicationController
   def new
+    @event = Event.new
+  end
+  def create
+    Event.create(event_params) 
+  end
+  private
+  def event_params
+    params.expect(event: [ :name ])
   end
 end
