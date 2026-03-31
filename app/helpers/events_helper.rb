afc122b59c48a08807b9099fef1e2ab2c2172abb
Godsila <sil@odds.team>
2026-03-31 22:12:28 +0700

    FIX: Move format_date_range to application helper and call function from detail and me

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,13 +1,2 @@
 module EventsHelper
-  def format_date_range(start_date, end_date)
-    return "" if start_date.blank? || end_date.blank?
-
-    if start_date == end_date
-      start_date.strftime('%d %b %Y')
-    elsif start_date.month == end_date.month && start_date.year == end_date.year
-      "#{start_date.strftime('%d')} - #{end_date.strftime('%d %b %Y')}"
-    else
-      "#{start_date.strftime('%d %b %Y')} - #{end_date.strftime('%d %b %Y')}"
-    end
-  end
 end
